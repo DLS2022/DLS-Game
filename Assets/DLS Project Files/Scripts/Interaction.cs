327ebfc00e7de40bef64e758a33dcc087d7ba13d
Killstrike25 <61766376+Killstrike25@users.noreply.github.com>
2022-04-07 12:49:06 -0400

    -Basic Player Interaction
    -Added a trigger for player interactions

---
+++ Assets/DLS Project Files/Scripts/Interaction.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    bool canInteract;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInteract)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Player Dialogue can be activated
+                Debug.Log("You hit the trigger");
+            } 
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canInteract = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canInteract = false;
+    }
+
+}
