e3b9141836dbf9ca3829b44a989d7c068c607c6a
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2022-04-08 07:24:32 -0700

    Got the Yarnspinner working, kinda.
    Can't figure out how to get commands working with this new version. Very frustrating.

--- Assets/DLS Project Files/Scripts/Interaction.cs
+++ Assets/DLS Project Files/Scripts/Interaction.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
+using StarterAssets;
 
 public class Interaction : MonoBehaviour
 {
+
+    [Header("Yarn Components")]
+    public YarnProject yproj;
+    public string talktonode;
+    public DialogueRunner diaRunner;
+
+    public GameObject player;
+    public FirstPersonController fpcscript;
+    public StarterAssetsInputs saiscript;
+
+    // private bool isCurrentlySpeaking;
     bool canInteract;
+
+    void Awake()
+    {
+        diaRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        fpcscript = player.GetComponent<FirstPersonController>();
+        saiscript = player.GetComponent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
@@ -16,13 +37,27 @@ public class Interaction : MonoBehaviour
     {
         if (canInteract)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && !diaRunner.IsDialogueRunning)
             {
                 //Player Dialogue can be activated
                 Debug.Log("You hit the trigger");
+                StartConversation();
             } 
         }
 
+        if(diaRunner.IsDialogueRunning)
+        {
+            fpcscript.enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            // Debug.Log("here in update");
+            fpcscript.enabled = true;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,4 +70,8 @@ public class Interaction : MonoBehaviour
         canInteract = false;
     }
 
+    private void StartConversation()
+    {
+        diaRunner.StartDialogue(talktonode);
+    }
 }
