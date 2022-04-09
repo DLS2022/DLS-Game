2cbc8cf08d6ee0350e96314093ae9e2d0af426a0
thealexanderkuang <49510751+thealexanderkuang@users.noreply.github.com>
2022-04-08 17:53:46 -0700

    Yarnspinner back up and running baybee

--- Assets/DLS Project Files/Scripts/Interaction.cs
+++ Assets/DLS Project Files/Scripts/Interaction.cs
@@ -1,30 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
+using StarterAssets;
 
 public class Interaction : MonoBehaviour
 {
+
+    [Header("Yarn Components")]
+    public YarnProject yproj;
+    public string talktonode;
+    public DialogueRunner diaRunner;
+
+    public GameObject player;
+    public FirstPersonController fpcscript;
+
+    // private bool isCurrentlySpeaking;
     bool canInteract;
+
+    void Awake()
+    {
+        diaRunner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        fpcscript = player.GetComponent<FirstPersonController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (canInteract)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && !diaRunner.IsDialogueRunning)
             {
                 //Player Dialogue can be activated
                 Debug.Log("You hit the trigger");
+                StartConversation();
             } 
         }
 
+        if(diaRunner.IsDialogueRunning)
+        {
+            fpcscript.enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            // Debug.Log("here in update");
+            fpcscript.enabled = true;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,4 +68,8 @@ public class Interaction : MonoBehaviour
         canInteract = false;
     }
 
+    private void StartConversation()
+    {
+        diaRunner.StartDialogue(talktonode);
+    }
 }
